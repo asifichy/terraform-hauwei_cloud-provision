07a8a542d0511f0ce20cbc42fe875bd4592fda8a
asifichy <asifimtiyajchowdhury@gmail.com>
2024-08-15 20:22:29 +0600

    fixing sensitive data

--- AS/variables.tf
+++ AS/variables.tf
@@ -1,10 +1,10 @@
 variable "huaweicloud_access_key" {
-  description = "WBBBUU15WI52O24CZDVS"
+  description = "your ak"
   type        = string
 }
 
 variable "huaweicloud_secret_key" {
-  description = "XQpd2Y7pgbt2bCPjUjUeoi6f7t9PGjXkv5KkRKo3"
+  description = "your sk"
   type        = string
 }
 /*
